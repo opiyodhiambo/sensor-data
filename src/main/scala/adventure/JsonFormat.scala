046ae85db6f3d8201da01daa5421d512369a48af
opiyodhiambo <rnldlinus@gmail.com>
2023-09-11 13:31:44 +0300

    updated the JSOn formatter

--- src/main/scala/adventure/JsonFormat.scala
+++ src/main/scala/adventure/JsonFormat.scala
@@ -9,21 +9,6 @@ import com.sksamuel.avro4s.AvroSchema
 
 import spray.json._
 
-case class SensorData(deviceId: UUID, timestamp: Instant, measurements: Measurements) extends SpecificRecordBase {
-  val schema: Schema = AvroSchema[SensorData]
-  val schemaJson: String = schema.toString
-
-  def get(field: Int): Object = field match {
-    case 0 => deviceId.asInstanceOf[AnyRef]
-    case 1 => timestamp.asInstanceOf[AnyRef] 
-    case 2 => measurements.asInstanceOf[AnyRef]
-    case _ => throw new AvroRuntimeException("invalid field index")
-  }
-  def getSchema(): org.apache.avro.Schema = SensorData.SCHEMA$
-  def put(field: Int, value: Object): Unit = ???
-}
-case class Measurements(power: Double, rotorSpeed: Double, windspeed: Double)
-
 trait UUIDJsonSupport extends DefaultJsonProtocol {
   implicit object UUIDFormat extends JsonFormat[UUID] {
     def write(uuid: UUID) = JsString(uuid.toString)
