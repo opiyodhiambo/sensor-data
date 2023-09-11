2d48fbfd70e77b5389a4f1d23b11c71c76fd4119
opiyodhiambo <rnldlinus@gmail.com>
2023-09-11 11:02:04 +0300

    declared the streamlet flow

--- src/main/scala/adventure/MetricsValidation.scala
+++ src/main/scala/adventure/MetricsValidation.scala
@@ -4,6 +4,9 @@ import cloudflow.akkastream._
 import cloudflow.streamlets._
 import cloudflow.streamlets.avro.AvroInlet
 import cloudflow.akkastream.scaladsl.FlowWithCommittableContext
+import cloudflow.akkastream.scaladsl.RunnableGraphStreamletLogic
+import akka.stream.scaladsl.RunnableGraph
+import cloudflow.akkastream.util.scaladsl.Splitter
 
 class MetricsValidation extends AkkaStreamlet {
   val in: CodecInlet[Metric] = AvroInlet[Metric]("in")
@@ -20,5 +23,7 @@ class MetricsValidation extends AkkaStreamlet {
       
     }
   }
-  override protected def createLogic(): AkkaStreamletLogic = ???
+  override protected def createLogic(): AkkaStreamletLogic = new RunnableGraphStreamletLogic() {
+    override def runnableGraph(): RunnableGraph[_] = sourceWithCommittableContext(in).to(Splitter.sink(flow, valid, invalid))
+  }
 }
