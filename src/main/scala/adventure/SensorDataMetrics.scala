5419963b01e88cfd915d58f403242e0c48aaaa60
opiyodhiambo <rnldlinus@gmail.com>
2023-09-11 10:10:14 +0300

    Defined the steamlet inlet and outlets

--- src/main/scala/adventure/SensorDataMetrics.scala
+++ src/main/scala/adventure/SensorDataMetrics.scala
@@ -3,11 +3,12 @@ import akka.stream.scaladsl._
 import cloudflow.akkastream.scaladsl._
 import cloudflow.streamlets._
 import cloudflow.akkastream._
+import cloudflow.streamlets.avro._
 
 class SensorDataMetrics extends AkkaStreamlet {
-  val in: CodecOutlet[SensorData] = ???
-  val out: CodecOutlet[Metric] = ???
-  override def shape(): StreamletShape = ???
+  val in: CodecOutlet[SensorData] = AvroInlet[SensorData]("in")
+  val out: CodecOutlet[Metric] = AvroInlet[Metric]("out").withPartitioner(RoundRobinPartitioner)
+  override def shape(): StreamletShape = 
 
   def flow = ???
   override protected def createLogic(): AkkaStreamletLogic = {
