f2cf2795e4bd9b821644e5d1697608de44482dde
opiyodhiambo <rnldlinus@gmail.com>
2023-09-11 10:59:57 +0300

    declared the metrics validation flow

--- src/main/scala/adventure/MetricsValidation.scala
+++ src/main/scala/adventure/MetricsValidation.scala
@@ -3,6 +3,7 @@ package adventure
 import cloudflow.akkastream._
 import cloudflow.streamlets._
 import cloudflow.streamlets.avro.AvroInlet
+import cloudflow.akkastream.scaladsl.FlowWithCommittableContext
 
 class MetricsValidation extends AkkaStreamlet {
   val in: CodecInlet[Metric] = AvroInlet[Metric]("in")
@@ -11,6 +12,13 @@ class MetricsValidation extends AkkaStreamlet {
 
   override val shape: StreamletShape = StreamletShape(in).withOutlets(valid, invalid)
 
-  def flow = ???
+  def flow = {
+    FlowWithCommittableContext[Metric]()
+      .map{metric =>
+        if (!SensorDataUtils.isInvalidMetric(metric)) Left(InvalidMetric(metric, "All measurements have to be positive"))
+        else Right(metric)
+      
+    }
+  }
   override protected def createLogic(): AkkaStreamletLogic = ???
 }
