98b14b71cd4b01aa2a9258c2fc72edc5f2f5f587
opiyodhiambo <rnldlinus@gmail.com>
2023-09-11 16:41:32 +0300

    updated the code layout

--- src/main/scala/sensordata/InvalidMetricLogger.scala
+++ src/main/scala/sensordata/InvalidMetricLogger.scala
@@ -1,26 +1,23 @@
 package sensordata
 
-import cloudflow.akkastream.AkkaStreamlet
-import cloudflow.akkastream.AkkaStreamletLogic
-import cloudflow.streamlets.CodecInlet
-import cloudflow.streamlets.StreamletShape
-import cloudflow.akkastream.scaladsl.FlowWithCommittableContext
-import cloudflow.akkastream.scaladsl.RunnableGraphStreamletLogic
-import akka.stream.scaladsl.RunnableGraph
+import akka.stream.scaladsl._
+import cloudflow.akkastream._
+import cloudflow.akkastream.scaladsl._
+import cloudflow.streamlets._
+import cloudflow.streamlets.avro._
 
 class InvalidMetricLogger extends AkkaStreamlet {
-  val inlet: CodecInlet[InvalidMetric] = CodecInlet[InvalidMetric]("inlet")
-  override val shape: StreamletShape = StreamletShape.withInlets(inlet)
-  val flow = FlowWithCommittableContext[InvalidMetric]()
-    .map { invalidMetric =>
-      system.log.warning("invalid metric detected", $invalidMetric)
-      invalidMetric
-    }
+  val inlet: CodecInlet[InvalidMetric] = AvroInlet[InvalidMetric]("in")
+  override val shape: StreamletShape   = StreamletShape.withInlets(inlet)
 
-  override def createLogic(): AkkaStreamletLogic =
-    new RunnableGraphStreamletLogic() {
-      override def runnableGraph(): RunnableGraph[_] =
-        sourceWithCommittableContext(inlet).via(flow).to(committableSink)
-    }
+  override def createLogic(): AkkaStreamletLogic = new RunnableGraphStreamletLogic() {
+    val flow = FlowWithCommittableContext[InvalidMetric]()
+      .map { invalidMetric =>
+        system.log.warning(s"Invalid metric detected! $invalidMetric")
+        invalidMetric
+      }
 
+    override def runnableGraph(): RunnableGraph[_] =
+      sourceWithCommittableContext(inlet).via(flow).to(committableSink)
+  }
 }
